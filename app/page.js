b65c5c6b2d0673fb685093fb210a71a5412816cd
PraveenBimsara <praveenbimsara99@gmail.com>
2024-08-15 12:37:28 +0530

    Created todo table

--- app/page.js
+++ app/page.js
@@ -1,11 +1,29 @@
-import Image from "next/image";
+"use client"
+import Todo from "@/components/Todo";
+import { useState } from "react";
 
 export default function Home() {
+    const [formData, setFormData] = useState({
+        title:"",
+        description:"",
+    });
+
+    const onChangeHandler = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setFormData(form=>({...form,[name]:value}));
+        console.log(formData);
+    }
+
+    const onSubmitHandler = async(e) => {
+        e.preventDefault();
+    }
+
   return (
     <>
-    <form className="flex items-start flex-col gap-2 w-[80%] max-w-[600px] mt-24 px-2 mx-auto">
-      <input type="text" name="title" placeholder="Enter Title" className="px-3 py-2 border-2 w-full"/>
-      <textarea name="description" placeholder="Enter Description" className="px-3 py-2 border-2 w-full"></textarea>
+    <form onSubmit={onSubmitHandler} className="flex items-start flex-col gap-2 w-[80%] max-w-[600px] mt-24 px-2 mx-auto">
+      <input value={formData.title} onChange={onChangeHandler} type="text" name="title" placeholder="Enter Title" className="px-3 py-2 border-2 w-full"/>
+      <textarea value={formData.description} onChange={onChangeHandler} name="description" placeholder="Enter Description" className="px-3 py-2 border-2 w-full"></textarea>
       <button type="submit" className="bg-orange-600 py-3 px-11 text-white font-bold">Add Todo</button>
     </form>
 
@@ -16,62 +34,26 @@ export default function Home() {
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
                 <th scope="col" className="px-6 py-3">
-                    Product name
+                    Id
                 </th>
                 <th scope="col" className="px-6 py-3">
-                    Color
+                    Title
                 </th>
                 <th scope="col" className="px-6 py-3">
-                    Category
+                    Description
                 </th>
                 <th scope="col" className="px-6 py-3">
-                    Price
+                    Status
+                </th>
+                <th scope="col" className="px-6 py-3">
+                    Action
                 </th>
             </tr>
         </thead>
         <tbody>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Apple MacBook Pro 17"
-                </th>
-                <td className="px-6 py-4">
-                    Silver
-                </td>
-                <td className="px-6 py-4">
-                    Laptop
-                </td>
-                <td className="px-6 py-4">
-                    $2999
-                </td>
-            </tr>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Microsoft Surface Pro
-                </th>
-                <td className="px-6 py-4">
-                    White
-                </td>
-                <td className="px-6 py-4">
-                    Laptop PC
-                </td>
-                <td className="px-6 py-4">
-                    $1999
-                </td>
-            </tr>
-            <tr className="bg-white dark:bg-gray-800">
-                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Magic Mouse 2
-                </th>
-                <td className="px-6 py-4">
-                    Black
-                </td>
-                <td className="px-6 py-4">
-                    Accessories
-                </td>
-                <td className="px-6 py-4">
-                    $99
-                </td>
-            </tr>
+            <Todo/>
+            <Todo/>
+            <Todo/>
         </tbody>
     </table>
 </div>
