99944e0519b89ffac397d0f8f6348bfa40a988f2
PraveenBimsara <praveenbimsara99@gmail.com>
2024-08-15 13:57:37 +0530

    Connect MongoDB

--- app/api/route.js
+++ app/api/route.js
@@ -1,4 +1,10 @@
+import { connectDB } from "@/lib/config/db";
 import { NextResponse } from "next/server";
+
+const LoadDB = async() => {
+    await connectDB();
+}
+LoadDB();
 
 export async function GET(request) {
     return NextResponse.json({msg:'get method hit'})
